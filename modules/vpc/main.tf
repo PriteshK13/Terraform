861ec64c7469dca715bec380818baac674f2dea8
Pritesh Kamble <priteshkamble13@gmail.com>
2026-04-06 16:30:32 +0530

    igw add

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,3 +5,11 @@ resource "aws_vpc" "main" {
     Name = var.vpc_name
    }
 }
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id + aws_vpc.main.ID
+
+    tags = {
+        Name = "${var.vpc_name}-igw
+    }
+}
